e77cac523f533773ca7ed16aeea91ce79441664d
FrankR-7 <franklinrosales7@gmail.com>
2020-09-18 19:55:38 -0600

    Added todo list

---
+++ todo/precache-manifest.17d0e6db25c1c718ceab0e491e0dd7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d34420d3af974a51d31d41c1f519c0",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c24c542e0ae2b3362499",
+    "url": "/todo/static/css/main.fc75a651.chunk.css"
+  },
+  {
+    "revision": "359ded17e018e630f381",
+    "url": "/todo/static/js/2.a3afdf85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.a3afdf85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c24c542e0ae2b3362499",
+    "url": "/todo/static/js/main.fc3f28a6.chunk.js"
+  },
+  {
+    "revision": "dd2b2f03a8e242cf6d39",
+    "url": "/todo/static/js/runtime-main.6ec96064.js"
+  }
+]);
